2789a22691aac23f1567e6451149ec1465279dd5
Col-E <mcoley2@gmu.edu>
2022-12-15 08:39:07 -0500

    Fix class info builder failing on java/lang/Object

--- recaf-api/src/main/java/software/coley/recaf/info/builder/AbstractClassInfoBuilder.java
+++ recaf-api/src/main/java/software/coley/recaf/info/builder/AbstractClassInfoBuilder.java
@@ -249,7 +249,7 @@ public abstract class AbstractClassInfoBuilder<B extends AbstractClassInfoBuilde
 	protected void verify() {
 		if (name == null)
 			throw new IllegalArgumentException("Name required");
-		if (superName == null && !access.hasAnnotationModifier())
+		if (superName == null && !access.hasAnnotationModifier() && !name.equals("java/lang/Object"))
 			throw new IllegalArgumentException("Super-name required");
 	}
 
