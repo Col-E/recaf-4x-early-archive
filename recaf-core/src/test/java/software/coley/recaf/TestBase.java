5259fe37439de59b8e8f24e1f818c70c05fc1d40
Col-E <mcoley2@gmu.edu>
2022-12-16 01:25:55 -0500

    It seems like JUnit 5's Isolated actually covers our use case.

--- recaf-core/src/test/java/software/coley/recaf/TestBase.java
+++ recaf-core/src/test/java/software/coley/recaf/TestBase.java
@@ -1,11 +1,13 @@
 package software.coley.recaf;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.parallel.Isolated;
 import software.coley.recaf.workspace.WorkspaceManager;
 
 /**
  * Common base for test classes using the recaf application.
  */
+@Isolated
 public class TestBase {
 	protected static final Recaf recaf = Bootstrap.get();
 	protected static WorkspaceManager workspaceManager;
