4b290f6935c1031bfe26c0643e9aecadda77e5a9
Col-E <mcoley2@gmu.edu>
2022-12-24 02:50:31 -0500

    Make results synchronized
    thank you xdark

--- recaf-api/src/main/java/software/coley/recaf/services/search/result/Results.java
+++ recaf-api/src/main/java/software/coley/recaf/services/search/result/Results.java
@@ -2,6 +2,7 @@ package software.coley.recaf.services.search.result;
 
 import software.coley.collections.delegate.DelegatingSortedSet;
 
+import java.util.Collections;
 import java.util.TreeSet;
 
 /**
@@ -14,6 +15,6 @@ public class Results extends DelegatingSortedSet<Result<?>> {
 	 * New results backed by tree-set.
 	 */
 	public Results() {
-		super(new TreeSet<>());
+		super(Collections.synchronizedNavigableSet(new TreeSet<>()));
 	}
 }
