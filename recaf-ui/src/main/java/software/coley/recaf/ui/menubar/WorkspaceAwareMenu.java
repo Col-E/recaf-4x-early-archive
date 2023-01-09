e0844a6c3dc0e2b22866030ff8684b033f54b9b1
Col-E <mcoley2@gmu.edu>
2023-01-08 20:16:49 -0500

    Create menu base that is aware of the current workspace state via properties

---
+++ recaf-ui/src/main/java/software/coley/recaf/ui/menubar/WorkspaceAwareMenu.java
@@ -0,0 +1,44 @@
+package software.coley.recaf.ui.menubar;
+
+import jakarta.annotation.Nonnull;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.scene.control.Menu;
+import software.coley.recaf.cdi.AutoRegisterWorkspaceListeners;
+import software.coley.recaf.workspace.WorkspaceCloseListener;
+import software.coley.recaf.workspace.WorkspaceOpenListener;
+import software.coley.recaf.workspace.model.Workspace;
+import software.coley.recaf.workspace.model.resource.AgentServerRemoteVmResource;
+import software.coley.recaf.workspace.model.resource.WorkspaceResource;
+
+/**
+ * {@link Menu} that is aware of the current workspace environment.
+ * <br>
+ * Implementations must be scoped beans.
+ *
+ * @author Matt Coley
+ */
+@AutoRegisterWorkspaceListeners
+public abstract class WorkspaceAwareMenu extends Menu implements WorkspaceOpenListener, WorkspaceCloseListener {
+	protected final BooleanProperty hasWorkspace = new SimpleBooleanProperty(false);
+	protected final BooleanProperty hasAgentWorkspace = new SimpleBooleanProperty(false);
+
+	@Override
+	public void onWorkspaceClosed(@Nonnull Workspace workspace) {
+		hasWorkspace.set(false);
+		hasAgentWorkspace.set(false);
+		workspaceStateChanged();
+	}
+
+	@Override
+	public void onWorkspaceOpened(@Nonnull Workspace workspace) {
+		WorkspaceResource primaryResource = workspace.getPrimaryResource();
+		hasWorkspace.set(true);
+		hasAgentWorkspace.set(primaryResource instanceof AgentServerRemoteVmResource);
+		workspaceStateChanged();
+	}
+
+	protected void workspaceStateChanged() {
+		// no-op by default
+	}
+}
