d5afcd99c5dc262fc99ee992bd834b86f5702d46
Col-E <mcoley2@gmu.edu>
2023-01-12 03:49:06 -0500

    Add recent files config
    For file menu and other IO prompts

---
+++ recaf-ui/src/main/java/software/coley/recaf/ui/config/RecentFilesConfig.java
@@ -0,0 +1,235 @@
+package software.coley.recaf.ui.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import software.coley.collections.Lists;
+import software.coley.observables.ObservableCollection;
+import software.coley.observables.ObservableInteger;
+import software.coley.observables.ObservableObject;
+import software.coley.observables.ObservableString;
+import software.coley.recaf.config.BasicCollectionConfigValue;
+import software.coley.recaf.config.BasicConfigContainer;
+import software.coley.recaf.config.BasicConfigValue;
+import software.coley.recaf.workspace.model.Workspace;
+import software.coley.recaf.workspace.model.resource.WorkspaceDirectoryResource;
+import software.coley.recaf.workspace.model.resource.WorkspaceFileResource;
+import software.coley.recaf.workspace.model.resource.WorkspaceResource;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Config for tracking recent file interactions.
+ *
+ * @author Matt Coley
+ */
+@ApplicationScoped
+public class RecentFilesConfig extends BasicConfigContainer {
+	public static final String ID = "recent-workspaces";
+	private final ObservableInteger maxRecentWorkspaces = new ObservableInteger(10);
+	private final ObservableCollection<WorkspaceModel, List<WorkspaceModel>> recentWorkspaces = new ObservableCollection<>(ArrayList::new);
+	private final ObservableString lastWorkspaceOpenDirectory = new ObservableString(System.getProperty("user.dir"));
+	private final ObservableString lastWorkspaceExportDirectory = new ObservableString(System.getProperty("user.dir"));
+
+	@Inject
+	public RecentFilesConfig() {
+		super(ID + CONFIG_SUFFIX);
+		// Add values
+		addValue(new BasicConfigValue<>("max-recent-workspaces", Integer.class, maxRecentWorkspaces));
+		addValue(new BasicCollectionConfigValue<>("recent-workspaces", List.class, WorkspaceModel.class, recentWorkspaces));
+		addValue(new BasicConfigValue<>("last-workspace-open-path", String.class, lastWorkspaceOpenDirectory));
+		addValue(new BasicConfigValue<>("last-workspace-export-path", String.class, lastWorkspaceExportDirectory));
+	}
+
+	/**
+	 * @param workspace
+	 * 		Workspace to add to {@link #getRecentWorkspaces()}.
+	 */
+	public void addWorkspace(Workspace workspace) {
+		// Wrap to model
+		ResourceModel primary = ResourceModel.from(workspace.getPrimaryResource());
+		List<ResourceModel> libraries = workspace.getSupportingResources().stream()
+				.map(ResourceModel::from)
+				.toList();
+		WorkspaceModel workspaceModel = new WorkspaceModel(primary, libraries);
+
+		// Update value
+		recentWorkspaces.add(workspaceModel);
+	}
+
+	/**
+	 * Refresh available workspaces, removing any items that cannot be loaded from the list.
+	 *
+	 * @see WorkspaceModel#canLoadWorkspace()
+	 */
+	public void refreshWorkspaces() {
+		List<WorkspaceModel> current = recentWorkspaces.getValue();
+		List<WorkspaceModel> loadable = current.stream()
+				.filter(WorkspaceModel::canLoadWorkspace)
+				.toList();
+		if (current.size() != loadable.size()) {
+			recentWorkspaces.setValue(loadable);
+		}
+	}
+
+	/**
+	 * @return Number of recent items to track.
+	 */
+	public ObservableInteger getMaxRecentWorkspaces() {
+		return maxRecentWorkspaces;
+	}
+
+	/**
+	 * @return Recent workspaces.
+	 */
+	public ObservableObject<List<WorkspaceModel>> getRecentWorkspaces() {
+		return recentWorkspaces;
+	}
+
+	/**
+	 * @return Last path used to open a workspace with.
+	 */
+	public ObservableString getLastWorkspaceOpenDirectory() {
+		return lastWorkspaceOpenDirectory;
+	}
+
+	/**
+	 * @return Last path used to export a workspace to.
+	 */
+	public ObservableString getLastWorkspaceExportDirectory() {
+		return lastWorkspaceExportDirectory;
+	}
+
+	/**
+	 * Basic wrapper for workspaces.
+	 *
+	 * @author Matt Coley
+	 * @see ResourceModel
+	 */
+	public static class WorkspaceModel {
+		private final ResourceModel primary;
+		private final List<ResourceModel> libraries;
+
+		/**
+		 * @param primary
+		 * 		Primary resource of the workspace.
+		 * @param libraries
+		 * 		Workspace supporting libraries.
+		 */
+		public WorkspaceModel(ResourceModel primary, List<ResourceModel> libraries) {
+			this.primary = primary;
+			this.libraries = libraries;
+		}
+
+		/**
+		 * @return Primary resource of the workspace.
+		 */
+		public ResourceModel getPrimary() {
+			return primary;
+		}
+
+		/**
+		 * @return Workspace supporting libraries.
+		 */
+		public List<ResourceModel> getLibraries() {
+			return libraries;
+		}
+
+		/**
+		 * @return {@code true} when the files still exist at their expected locations.
+		 */
+		public boolean canLoadWorkspace() {
+			Path path = Paths.get(getPrimary().getPath());
+			if (!Files.exists(path))
+				return false;
+			for (ResourceModel model : getLibraries()) {
+				path = Paths.get(model.getPath());
+				if (!Files.exists(path))
+					return false;
+			}
+			return true;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			WorkspaceModel that = (WorkspaceModel) o;
+			return primary.equals(that.primary) && libraries.equals(that.libraries);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(primary, libraries);
+		}
+	}
+
+	/**
+	 * Wrapper for a resources content source path.
+	 *
+	 * @author Matt Coley
+	 */
+	public static class ResourceModel {
+		private final String path;
+
+		/**
+		 * @param path
+		 * 		Path to the resource content source.
+		 */
+		public ResourceModel(String path) {
+			this.path = path;
+		}
+
+		/**
+		 * @param resource
+		 * 		Some resource sourced from a file or directory.
+		 *
+		 * @return Representation of the content source.
+		 */
+		public static ResourceModel from(WorkspaceResource resource) {
+			if (resource instanceof WorkspaceFileResource fileResource) {
+				return new ResourceModel(fileResource.getFileInfo().getName());
+			} else if (resource instanceof WorkspaceDirectoryResource fileResource) {
+				return new ResourceModel(fileResource.getDirectoryPath().toAbsolutePath().toString());
+			}
+			throw new UnsupportedOperationException("Cannot serialize content source of type: " +
+					resource.getClass().getName());
+		}
+
+		/**
+		 * @return Shortened path of resource's content source.
+		 */
+		public String getSimpleName() {
+			String name = path;
+			int slashIndex = name.lastIndexOf('/');
+			if (slashIndex > 0)
+				name = name.substring(slashIndex + 1);
+			return name;
+		}
+
+		/**
+		 * @return Path to the resource content source. Can be a file path, maven coordinates, or url.
+		 */
+		public String getPath() {
+			return path;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			ResourceModel that = (ResourceModel) o;
+			return path.equals(that.path);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(path);
+		}
+	}
+}
