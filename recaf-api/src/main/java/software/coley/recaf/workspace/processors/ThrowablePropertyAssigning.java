c99785fa3e690d638f2dc174452d1b3e09c1037a
Col-E <mcoley2@gmu.edu>
2023-02-13 20:21:26 -0500

    Add post-processor for marking throwable types

---
+++ recaf-api/src/main/java/software/coley/recaf/workspace/processors/ThrowablePropertyAssigning.java
@@ -0,0 +1,40 @@
+package software.coley.recaf.workspace.processors;
+
+import jakarta.annotation.Nonnull;
+import jakarta.inject.Inject;
+import software.coley.recaf.cdi.WorkspaceScoped;
+import software.coley.recaf.info.ClassInfo;
+import software.coley.recaf.info.properties.builtin.ThrowableProperty;
+import software.coley.recaf.services.inheritance.InheritanceGraph;
+import software.coley.recaf.workspace.WorkspaceProcessor;
+import software.coley.recaf.workspace.model.Workspace;
+
+/**
+ * Workspace processor that marks {@link ClassInfo} values that inherit from {@link Throwable}
+ * as having a {@link ThrowableProperty}. This allows instant look-ups for if a class is throwable,
+ * by bypassing repeated calls to {@link InheritanceGraph}.
+ *
+ * @author Matt Coley
+ */
+@WorkspaceScoped
+public class ThrowablePropertyAssigning implements WorkspaceProcessor {
+	private final InheritanceGraph graph;
+
+	@Inject
+	public ThrowablePropertyAssigning(InheritanceGraph graph) {
+		this.graph = graph;
+	}
+
+	@Override
+	public void onWorkspaceOpened(@Nonnull Workspace workspace) {
+		graph.getVertex("java/lang/Throwable").getAllChildren().forEach(vertex -> {
+			ClassInfo classInfo = vertex.getValue();
+			ThrowableProperty.set(classInfo);
+		});
+	}
+
+	@Override
+	public String name() {
+		return "Mark throwable types";
+	}
+}
