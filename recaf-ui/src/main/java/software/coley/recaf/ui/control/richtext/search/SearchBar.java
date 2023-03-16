d6eb12ec7d2aa10d29a9fdc4ace4dc51ca0899ae
Col-E <mcoley2@gmu.edu>
2023-03-16 18:32:09 -0400

    Support for regex group usage in replace field in search bar

--- recaf-ui/src/main/java/software/coley/recaf/ui/control/richtext/search/SearchBar.java
+++ recaf-ui/src/main/java/software/coley/recaf/ui/control/richtext/search/SearchBar.java
@@ -5,6 +5,7 @@ import atlantafx.base.controls.Popover;
 import atlantafx.base.controls.Spacer;
 import atlantafx.base.theme.Styles;
 import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import javafx.beans.property.SimpleBooleanProperty;
@@ -102,7 +103,7 @@ public class SearchBar implements EditorComponent, EventHandler<KeyEvent> {
 		private final CustomTextField replaceInput = new CustomTextField();
 		private final ObservableList<String> pastSearches = FXCollections.observableArrayList();
 		private final ObservableList<String> pastReplaces = FXCollections.observableArrayList();
-		private final ObservableList<IntRange> resultRanges = FXCollections.observableArrayList();
+		private final ObservableList<Match> resultRanges = FXCollections.observableArrayList();
 		private final HBox replaceLine;
 		private final Editor editor;
 
@@ -153,6 +154,7 @@ public class SearchBar implements EditorComponent, EventHandler<KeyEvent> {
 			// Create label to display number of results.
 			Label resultCount = new Label();
 			resultCount.setMinWidth(70);
+			resultCount.setAlignment(Pos.CENTER);
 			resultCount.setTextAlignment(TextAlignment.CENTER);
 			resultCount.textProperty().bind(lastResultIndex.map(n -> {
 				int i = n.intValue();
@@ -162,7 +164,7 @@ public class SearchBar implements EditorComponent, EventHandler<KeyEvent> {
 					return (i + 1) + "/" + resultRanges.size();
 				}
 			}));
-			resultRanges.addListener((ListChangeListener<IntRange>) c -> {
+			resultRanges.addListener((ListChangeListener<Match>) c -> {
 				if (resultRanges.isEmpty()) {
 					resultCount.setStyle("-fx-text-fill: red;");
 				} else {
@@ -287,7 +289,7 @@ public class SearchBar implements EditorComponent, EventHandler<KeyEvent> {
 
 			// Check for regex, then do a standard search if not regex.
 			String text = editor.getText();
-			List<IntRange> tempRanges = new ArrayList<>();
+			List<Match> tempRanges = new ArrayList<>();
 			if (regex.get()) {
 				// Validate the regex.
 				RegexUtil.RegexValidation validation = RegexUtil.validate(search);
@@ -296,7 +298,7 @@ public class SearchBar implements EditorComponent, EventHandler<KeyEvent> {
 					// It's valid, populate the ranges.
 					Matcher matcher = RegexUtil.getMatcher(search, text);
 					while (matcher.find())
-						tempRanges.add(new IntRange(matcher.start(), matcher.end()));
+						tempRanges.add(Match.match(matcher.start(), matcher.end(), matcher));
 				} else {
 					// It's not valid. Tell the user what went wrong.
 					popoverValidation = new Popover(new Label(validation.message()));
@@ -323,7 +325,7 @@ public class SearchBar implements EditorComponent, EventHandler<KeyEvent> {
 					int start = text.indexOf(search, i);
 					if (start > -1) {
 						int end = start + size;
-						tempRanges.add(new IntRange(start, end));
+						tempRanges.add(Match.match(start, end));
 						i = end;
 					} else {
 						break;
@@ -341,11 +343,11 @@ public class SearchBar implements EditorComponent, EventHandler<KeyEvent> {
 				int lastMatchedTerm = area.getSelectedText().length();
 				int caret = area.getCaretPosition();
 				int searchStart = Math.max(0, caret - lastMatchedTerm - 1);
-				int rangeIndex = CollectionUtil.sortedInsertIndex(resultRanges, new IntRange(searchStart, searchStart));
+				int rangeIndex = CollectionUtil.sortedInsertIndex(resultRanges, Match.match(searchStart, searchStart));
 				if (rangeIndex >= resultRanges.size())
 					rangeIndex = 0;
 				lastResultIndex.set(rangeIndex);
-				IntRange targetRange = resultRanges.get(rangeIndex);
+				IntRange targetRange = resultRanges.get(rangeIndex).range();
 				area.selectRange(targetRange.start(), targetRange.end());
 			}
 		}
@@ -365,13 +367,13 @@ public class SearchBar implements EditorComponent, EventHandler<KeyEvent> {
 			// Get the next range index by doing a search starting from the current caret position + 1.
 			CodeArea area = editor.getCodeArea();
 			int caret = area.getCaretPosition() + 1;
-			int rangeIndex = CollectionUtil.sortedInsertIndex(resultRanges, new IntRange(caret, caret));
+			int rangeIndex = CollectionUtil.sortedInsertIndex(resultRanges, Match.match(caret, caret));
 			if (rangeIndex >= resultRanges.size())
 				rangeIndex = 0;
 
 			// Set index & select the range.
 			lastResultIndex.set(rangeIndex);
-			IntRange range = resultRanges.get(rangeIndex);
+			IntRange range = resultRanges.get(rangeIndex).range();
 			area.selectRange(range.start(), range.end());
 		}
 
@@ -391,13 +393,13 @@ public class SearchBar implements EditorComponent, EventHandler<KeyEvent> {
 			// then go back by one position, wrapping around if necessary.
 			CodeArea area = editor.getCodeArea();
 			int caret = area.getCaretPosition();
-			int rangeIndex = CollectionUtil.sortedInsertIndex(resultRanges, new IntRange(caret - area.getSelectedText().length(), caret)) - 1;
+			int rangeIndex = CollectionUtil.sortedInsertIndex(resultRanges, Match.match(caret - area.getSelectedText().length(), caret)) - 1;
 			if (rangeIndex < 0)
 				rangeIndex = resultRanges.size() - 1;
 
 			// Set index & select the range.
 			lastResultIndex.set(rangeIndex);
-			IntRange range = resultRanges.get(rangeIndex);
+			IntRange range = resultRanges.get(rangeIndex).range();
 			area.selectRange(range.start(), range.end());
 		}
 
@@ -407,13 +409,10 @@ public class SearchBar implements EditorComponent, EventHandler<KeyEvent> {
 		private void replace() {
 			recordReplace();
 
-			String replacement = replaceInput.getText();
+			// Replace the current index.
 			int index = lastResultIndex.get();
-			if (index >= 0) {
-				IntRange range = resultRanges.get(index);
-				CodeArea area = editor.getCodeArea();
-				area.replaceText(range.start(), range.end(), replacement);
-			}
+			if (index >= 0)
+				replaceResult(index);
 		}
 
 		/**
@@ -424,21 +423,52 @@ public class SearchBar implements EditorComponent, EventHandler<KeyEvent> {
 
 			// Iterate backwards, replacing all matches.
 			// We record the start/end ranges so that we can do a re-style after for the affected range.
-			CodeArea area = editor.getCodeArea();
-			String replacement = replaceInput.getText();
-			List<IntRange> rangesCopy = new ArrayList<>(resultRanges);
+			List<Match> rangesCopy = new ArrayList<>(resultRanges);
 			int max = rangesCopy.size() - 1;
-			for (int i = max; i >= 0; i--) {
-				IntRange range = rangesCopy.get(i);
-				area.replaceText(range.start(), range.end(), replacement);
-			}
-			int start = rangesCopy.get(0).start();
-			int end = rangesCopy.get(max).end();
-			editor.restyleAtPosition(start, end - start);
+			for (int i = max; i >= 0; i--)
+				replaceResult(i);
 
-			// Update replacement history.
-			pastReplaces.remove(replacement);
-			pastReplaces.add(0, replacement);
+			// Restyle range from first match through last match.
+			int start = rangesCopy.get(0).range().start();
+			int end = rangesCopy.get(max).range().end();
+			editor.restyleAtPosition(start, end - start);
+		}
+
+		/**
+		 * Replaces the range in the {@link #resultRanges} at the given {@code index} with the text of {@link #replaceInput}.
+		 * However, if {@link #regex} is {@code true}, then transformations are made on the replacement text to support
+		 * regex groups.
+		 * <br>
+		 * For example:
+		 * <ul>
+		 *     <li>Search: {@code public (class)}</li>
+		 *     <li>Replace: {@code private $1}</li>
+		 *     <li>Yields: {@code private class}</li>
+		 * </ul>
+		 *
+		 * @param index
+		 * 		Index in {@link #resultRanges} to replace.
+		 */
+		private void replaceResult(int index) {
+			String replacement = replaceInput.getText();
+			CodeArea area = editor.getCodeArea();
+			Match match = resultRanges.get(index);
+			String[] groups = match.groups();
+			if (groups != null) {
+				// Record '$N' patterns in the string
+				//  - 0: whole matched text
+				//  - N: group N's text
+				Matcher matcher = RegexUtil.getMatcher("(?<!\\\\)(?:(\\\\\\\\)*)\\$\\d+", replacement);
+				while (matcher.find()) {
+					int groupId = Integer.parseInt(matcher.group(0).substring(1));
+					if (groupId < groups.length) {
+						String groupText = groups[groupId];
+						replacement = replacement.replace("$" + groupId, groupText);
+					}
+				}
+			}
+			IntRange range = match.range();
+			area.replaceText(range.start(), range.end(), replacement);
 		}
 
 		/**
@@ -512,6 +542,30 @@ public class SearchBar implements EditorComponent, EventHandler<KeyEvent> {
 		private void requestSearchFocus() {
 			searchInput.requestFocus();
 			searchInput.selectAll();
+		}
+	}
+
+	/**
+	 * @param range
+	 * 		Range of the match.
+	 * @param groups
+	 * 		Regex groups of the match, if the match is done with {@link Bar#regex} enabled.
+	 */
+	private record Match(@Nonnull IntRange range, @Nullable String[] groups) implements Comparable<Match> {
+		@Nonnull
+		static Match match(int start, int end) {
+			return new Match(new IntRange(start, end), null);
+		}
+
+		@Nonnull
+		static Match match(int start, int end, @Nonnull Matcher matcher) {
+			return new Match(new IntRange(start, end), matcher.groups());
+		}
+
+
+		@Override
+		public int compareTo(Match o) {
+			return range.compareTo(o.range);
 		}
 	}
 }
