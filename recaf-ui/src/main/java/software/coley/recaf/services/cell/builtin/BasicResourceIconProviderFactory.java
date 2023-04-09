51e531d98ac79032224f6098f59a9dee4f7f6c37
Col-E <mcoley2@gmu.edu>
2023-04-09 08:58:37 -0400

    Use Android icon for APK WorkspaceResource graphics

--- recaf-ui/src/main/java/software/coley/recaf/services/cell/builtin/BasicResourceIconProviderFactory.java
+++ recaf-ui/src/main/java/software/coley/recaf/services/cell/builtin/BasicResourceIconProviderFactory.java
@@ -7,6 +7,8 @@ import software.coley.recaf.services.cell.PackageIconProviderFactory;
 import software.coley.recaf.services.cell.ResourceIconProviderFactory;
 import software.coley.recaf.util.Icons;
 import software.coley.recaf.workspace.model.Workspace;
+import software.coley.recaf.workspace.model.resource.WorkspaceDirectoryResource;
+import software.coley.recaf.workspace.model.resource.WorkspaceFileResource;
 import software.coley.recaf.workspace.model.resource.WorkspaceResource;
 
 /**
@@ -16,12 +18,19 @@ import software.coley.recaf.workspace.model.resource.WorkspaceResource;
  */
 @ApplicationScoped
 public class BasicResourceIconProviderFactory implements ResourceIconProviderFactory {
-	private static final IconProvider PROVIDER = Icons.createProvider(Icons.FILE_JAR);
+	private static final IconProvider PROVIDER_JAR = Icons.createProvider(Icons.FILE_JAR);
+	private static final IconProvider PROVIDER_ANDROID = Icons.createProvider(Icons.ANDROID);
+	private static final IconProvider PROVIDER_DIR = Icons.createProvider(Icons.FOLDER);
 
 	@Nonnull
 	@Override
 	public IconProvider getResourceIconProvider(@Nonnull Workspace workspace,
 												@Nonnull WorkspaceResource resource) {
-		return PROVIDER;
+		if (resource instanceof WorkspaceDirectoryResource)
+			return PROVIDER_DIR;
+		if (resource instanceof WorkspaceFileResource fileResource)
+			if (fileResource.getFileInfo().getName().toLowerCase().endsWith(".apk"))
+				return PROVIDER_ANDROID;
+		return PROVIDER_JAR;
 	}
 }
