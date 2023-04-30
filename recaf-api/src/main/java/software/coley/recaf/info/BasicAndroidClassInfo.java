1c2424d9e83edda94da8ef8be0dfa5af91586d9f
Col-E <mcoley2@gmu.edu>
2023-04-30 12:06:44 -0400

    Sync Android --> Java conversions

--- recaf-api/src/main/java/software/coley/recaf/info/BasicAndroidClassInfo.java
+++ recaf-api/src/main/java/software/coley/recaf/info/BasicAndroidClassInfo.java
@@ -43,21 +43,29 @@ public class BasicAndroidClassInfo extends BasicClassInfo implements AndroidClas
 	@Override
 	public JvmClassInfo asJvmClass() {
 		if (converted == null) {
-			try {
-				String name = getName();
-				ApplicationData data = ApplicationData.fromProgramClasses(Collections.singleton(dexClass));
-				data.setOperationOptionsProvider(() -> new Options()
-						.enableLoadStoreOptimization()
-						.setLenient(true)
-						.setReplaceInvalidMethodBodies(true));
-				byte[] convertedBytecode = data.exportToJvmClass(name);
-				if (convertedBytecode == null)
-					throw new IllegalStateException("Failed to convert Dalvik model of " + name + " to JVM bytecode, " +
-							"conversion results did not include type name.");
-				ClassReader reader = new ClassReader(convertedBytecode);
-				converted = new JvmClassInfoBuilder(reader).build();
-			} catch (ConversionException | IOException ex) {
-				throw new IllegalStateException(ex);
+			// This is an expensive operation, so it's best to make other thread access wait until it is done and
+			// then use the singular return value.
+			synchronized (this) {
+				// If there are 2+ requests here, and we let one through to completion, when the others are let in
+				// this value should be computed then.
+				if (converted != null)
+					return converted;
+				try {
+					String name = getName();
+					ApplicationData data = ApplicationData.fromProgramClasses(Collections.singleton(dexClass));
+					data.setOperationOptionsProvider(() -> new Options()
+							.enableLoadStoreOptimization()
+							.setLenient(true)
+							.setReplaceInvalidMethodBodies(true));
+					byte[] convertedBytecode = data.exportToJvmClass(name);
+					if (convertedBytecode == null)
+						throw new IllegalStateException("Failed to convert Dalvik model of " + name + " to JVM bytecode, " +
+								"conversion results did not include type name.");
+					ClassReader reader = new ClassReader(convertedBytecode);
+					converted = new JvmClassInfoBuilder(reader).build();
+				} catch (ConversionException | IOException ex) {
+					throw new IllegalStateException(ex);
+				}
 			}
 		}
 		return converted;
